d94555c0f3fd29d389270e45dbdbb415c20e4313
ritamcouchbase <ritamcouchbase@gmail.com>
2016-07-25 10:28:16 +0530

    Added table for component wise for changelist and added color coding for percentage for failing > 10

--- daily_results_mail.py
+++ daily_results_mail.py
@@ -14,6 +14,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import json
 import sys
+import time
 
 def retrun_jira_24hrs():
     jira = JIRA({"server": "https://issues.couchbase.com"})
@@ -60,20 +61,20 @@ def query_build_results(version_number=None,build_no=None):
     print temp
     q = N1QLQuery(temp)
     for row in conn.n1ql_query(q):
-        print row  # {'age': .., 'lname': ..., 'fname': ...}
+        #print row  # {'age': .., 'lname': ..., 'fname': ...}
         if row['component'] not in top_components:
             top_components.append(row['component'])
             detail_component.append({'component':row['component'],'totalCount':row['totalCount'],'failCount':row['failCount']})
         else:
             for details in detail_component:
                 if (details['component'] == row['component']):
-                    details['totalCount'] = details['totalCount'] + row['totalCount']
-                    details['failCount'] = details['failCount'] + row ['failCount']
-    return detail_component
+                    details['totalCount'] += row['totalCount']
+                    details['failCount'] +=  row ['failCount']
+    return detail_component,sorted(top_components)
 
 
 def _construct_build_results_body(version_number,build_no):
-    component_detail = query_build_results(version_number,build_no)
+    component_detail, top_component = query_build_results(version_number,build_no)
     defect_body = "<table border='1'style='float: left' cellpadding='5' cellspacing='5'>"
     defect_body = defect_body + \
             "<tr> " + \
@@ -83,19 +84,37 @@ def _construct_build_results_body(version_number,build_no):
             "<th> Component </th>" + \
             "<th> Total </th>" + \
             "<th> Fail </th>" + \
+            "<th> Fail % </th>" + \
             "</tr>"
-    for details in component_detail:
-        defect_body = defect_body + "<tr>" + \
-            "<td>" + details['component'] + "</td>" + \
-            "<td>" + str(details['totalCount']) + "</td>" + \
-            "<td>" + str(details['failCount']) + "</td>" + "</tr>"
-    defect_body = defect_body + "</table>"
+    for i in range(0,len(top_component)):
+        for j in range(0,len(component_detail)):
+            if (component_detail[j]['component'] == top_component[i]):
+                temp = component_detail[j]
+                break;
+        percentage = int((float(temp['failCount'])/float(temp['totalCount']))*100)
+        defect_body += "<tr>" + \
+        "<td>" + temp['component'] + "</td>" + \
+        "<td>" + str(temp['totalCount']) + "</td>" + \
+        "<td>" + str(temp['failCount']) + "</td>"
+        if int(percentage) >= 10:
+            defect_body += "<td bgcolor='#FF33C7'>" + str(percentage) + "</td>" + "</tr>"
+        else:
+            defect_body += "<td>" + str(percentage) + "</td>" + "</tr>"
+    defect_body += "</table>"
     return defect_body
 
-def _get_change_list(start_build,end_build,version_no):
-    conn = urllib2.urlopen("http://172.23.123.43:8282/changelog?ver={0}&from={1}&to={2}".format(version_no, start_build, end_build))
-    ret = json.loads(conn.read())
+
+# "<td>" + details['component'] + "</td>" + \
+# "<td>" + str(details['totalCount']) + "</td>" + \
+# "<td>" + str(details['failCount']) + "</td>" + "</tr>"
+
+def _get_change_list(start_build,end_build,version_number):
     reformat = {}
+    top_repo = []
+    detail_repo = {}
+    conn = urllib2.urlopen("http://172.23.123.43:8282/changelog?ver={0}&from={1}&to={2}".format(version_number, start_build, end_build))
+    ret = json.loads(conn.read())
+
     for val in ret['log']:
         if not reformat.has_key(val['repo']):
             reformat[val['repo']] = []
@@ -106,40 +125,75 @@ def _get_change_list(start_build,end_build,version_no):
     for k in keys:
         val = reformat[k]
         for v in val:
+            if v['repo'] not in top_repo:
+                top_repo.append(v['repo'])
+                detail_repo[v['repo']] = v['message'][0:75] + "----" + v['committer']['name'] + "<br>"
+            else:
+                temp = detail_repo[v['repo']]
+                detail_repo[v['repo']] = temp + v['message'][0:75] + "----" + v['committer']['name'] + "<br>"
+
+    changes_body = "<table border='1'style='float: left' cellpadding='5' cellspacing='5'>"
+    changes_body = changes_body + \
+                  "<tr> " + \
+                  "<td colspan='2'> Changes between Build Number : {0} - {1}</td>".format(version_number + "-" + start_build, version_number + "-" + end_build) + \
+                  "</tr>" + \
+                  "<tr>" + \
+                  "<th> Projects </th>" + \
+                  "<th> Commit </th>" + \
+                  "</tr>"
+
+    for i in range(0,len(top_repo)):
+        changes_body += "<tr>" + \
+                       "<td>" + top_repo[i] + "</td>" + \
+                       "<td>" + detail_repo[top_repo[i]] + "</td>" + \
+                        "</tr>"
+    changes_body += "</table>"
+    return changes_body
+
+
+    '''
+    for k in keys:
+        val = reformat[k]
+        for v in val:
             ret1 += (v['message'])[0:75] + "----"
             ret1 += v['repo'] + "---"
             ret1 += str(''.join(v['fixes'])) + "---"
             ret1 += v['committer']['name'] + "---" + "<br>"
     ret1 += "<br>"
     return ret1
+    '''
 
 
+def _construct_email_body(current_build,lastbuild,secondlastbuild,version_number):
+    html = """\
+        <html>
+          <head></head>
+          <body>  \
+            <h1> Today Daily Build - {0} </h1>  \
+            <h1> Commit Between Build - {1} - {2}  </h1> """.format(version_number + "-" + current_build,
+                                                                    version_number + "-" + lastbuild,
+                                                                    version_number + "-" + current_build) + \
+           _get_change_list(lastbuild, current_build, version_number) + \
+           """<br> <br> <h1> Test Results for Build and Build </h1> <div>""" + \
+           _construct_build_results_body(version_number, lastbuild) + \
+           _construct_build_results_body(version_number, secondlastbuild) + \
+           """</div>
+         </body>
+       </html>
+       """
+    print html
+    return html
 
-def _send_email(current_build,lastbuild,secondlastbuild,version_number,password):
+def _send_email(current_build,last_build,sec_last_build,version_number,password):
     from_email = 'ritamcouchbase@gmail.com'
     to_email = 'ritam@couchbase.com'
     email_recipients = ['ritam@couchbase.com']
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = 'Test Subject'
+    msg['Subject'] = 'Daily Run Result'
     msg['From'] = from_email
     msg['To'] = ",".join(email_recipients)
-
-    html = """\
-    <html>
-      <head></head>
-      <body>  \
-        <h1> Today Daily Build - {0} </h1>  \
-        <h1> Commit Between Build - {1} - {2}  </h1> """.format(version_number+ "-"+ current_build,version_number + "-"+ lastbuild, version_number + "-"+ current_build) + \
-        _get_change_list(lastbuild,current_build,version_number) + \
-        """ <h1> Test Results for Build and Build </h1> <div>""" + \
-        _construct_build_results_body(version_number,lastbuild) + \
-        _construct_build_results_body(version_number,secondlastbuild) + \
-        """</div>
-      </body>
-    </html>
-    """
-
-    part2 = MIMEText(html, 'html')
+    email_body = _construct_email_body(current_build,last_build,sec_last_build,version_number)
+    part2 = MIMEText(email_body, 'html')
     msg.attach(part2)
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
@@ -149,9 +203,9 @@ def _send_email(current_build,lastbuild,secondlastbuild,version_number,password)
     server.quit()
 
 
+
 def main(argv):
     current_build = argv[0]
-    print current_build
     conn = _sdk_connection(bucket='default',host_ip="172.23.107.7")
     version_no = current_build[0:5]
     lastbuild = (conn.get('lastbuild').value['build_no']).split("-")[1]
@@ -159,8 +213,8 @@ def main(argv):
     current_build = current_build.split("-")[1]
     password = (conn.get('ritampass').value['password'])
     _send_email(current_build,lastbuild,secondlastbuild,version_no,password)
-    conn.upsert('lastbuild',{'build_no':version_no + "-" + current_build})
-    conn.upsert('secondlastbuild',{'build_no':version_no + "-" + lastbuild})
+    #conn.upsert('lastbuild',{'build_no':version_no + "-" + current_build})
+    #conn.upsert('secondlastbuild',{'build_no':version_no + "-" + lastbuild})
 
 
 if __name__ == "__main__":
